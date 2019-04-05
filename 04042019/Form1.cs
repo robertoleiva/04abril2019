41bfa7d505b4741667b79b2f481677df8d53eff2
robertoleiva <47877340+robertoleiva@users.noreply.github.com>
2019-04-04 20:01:57 -0600

    Add files via upload

---
+++ 04042019/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace _04042019
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            listBox1.Items.Clear();
+            DriveInfo[] u = DriveInfo.GetDrives();
+            foreach (DriveInfo unidad in u)
+
+            
+            try
+            {
+                listBox1.Items.Add(unidad.Name);
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show("ERROR DE LECTURA" + ex.Message);
+
+            }
+                 
+
+
+
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+
+        }
+
+        private void treeView2_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            //listBox2.Items.Clear();
+            String unidades=listBox1.SelectedItem.ToString();
+            listBox2.Items.Add(unidades);
+
+
+        }
+    }
+}
